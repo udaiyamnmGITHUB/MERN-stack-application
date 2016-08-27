be3d5f8dc29c90011f301358e8cad02ff1349afe
udaiyamnmGITHUB <udaiyamnm@gmail.com>
2016-08-28 00:43:55 +0530

    this commit is for introducing the session management in this app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+console.log("introducing the session management in this app");
 console.log("process.env.NODE_ENV:::"+ process.env.NODE_ENV);
-console.log("this commmit from the local");
+
 var express = require('express');
 var app = express();
 var appConfig = require("./app/config/app-config");
