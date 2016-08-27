5cd6b66c43557454e4c65d6a2e8bab784c154a26
Udai <udaiyamnm@gmail.com>
2016-08-28 00:33:59 +0530

    test commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 console.log("process.env.NODE_ENV:::"+ process.env.NODE_ENV);
-
+console.log("test commit");
 var express = require('express');
 var app = express();
 var appConfig = require("./app/config/app-config");
